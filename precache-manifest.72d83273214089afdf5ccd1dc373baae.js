345c9cc80f70265122d805638d370f3d041160e2
Alibek Manabayev <m-nny@yandex.ru>
2019-12-06 02:27:45 +0300

    Updates

---
+++ precache-manifest.72d83273214089afdf5ccd1dc373baae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/phoenix/favicon.ico"
+  },
+  {
+    "revision": "00d92cc9df3800f72bd3a16c080707e1",
+    "url": "/phoenix/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/phoenix/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/phoenix/manifest.json"
+  },
+  {
+    "revision": "e8de03df5ebe2c8adaf1",
+    "url": "/phoenix/static/js/main.e8de03df.chunk.js"
+  },
+  {
+    "revision": "ab416953c1cb65489758",
+    "url": "/phoenix/static/js/runtime~main.ab416953.js"
+  },
+  {
+    "revision": "3ca81432027bbb7b376c",
+    "url": "/phoenix/static/js/vendors~main.3ca81432.chunk.js"
+  }
+]);
